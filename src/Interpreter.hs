51d1796d8a69fda7d5c66d209777c14ebaae3f8b
Luca Padovani <luca.padovani@unito.it>
2021-12-28 15:26:15 +0100

    better representation of session endpoints

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -30,23 +30,26 @@ import Process
 import Exceptions
 import Render
 
--- | A session endpoint is a pair of channel, the first one is used
--- for receiving messages and the second one for sending
--- messages. The peer endpoint has the same two channels in the
--- opposite order.
-type Session = (Chan Message, Chan Message)
+-- | A session endpoint consists of a symbolic and a pair of
+-- channels, the first one is used for receiving messages and the
+-- second one for sending messages. The peer endpoint has the same
+-- two channels in the opposite order.
+data Endpoint = Endpoint ChannelName (Chan Message) (Chan Message)
+
+instance Show Endpoint where
+  show (Endpoint u _ _) = showWithPos u
 
 -- | Representation of messages.
 data Message
   -- | Termination signal.
   = CloseM
   -- | Delegation.
-  | SessionM Session
+  | EndpointM Endpoint
   -- | Label message.
   | LabelM Label
 
 -- | An __environment__ maps channel names to session endpoints.
-type Environment = Map ChannelName Session
+type Environment = Map ChannelName Endpoint
 
 -- | Map from process names to bound names and process body.
 type ProcessMap = Map ProcessName ([ChannelName], Process)
@@ -70,9 +73,9 @@ pick m (n : ns) = 1 + pick (m - n) ns
 run :: Bool -> [ProcessDef] -> Process -> IO ()
 run logging pdefs = aux Map.empty
   where
-    channel :: Environment -> ChannelName -> Session
-    channel env u | Just c <- Map.lookup u env = c
-                  | otherwise = runtimeError $ "unbound channel name " ++ show u
+    find :: Environment -> ChannelName -> Endpoint
+    find env u | Just c <- Map.lookup u env = c
+               | otherwise = runtimeError $ "unbound channel name " ++ show u
 
     children :: MVar [MVar ()]
     children = unsafePerformIO (newMVar [])
@@ -80,11 +83,17 @@ run logging pdefs = aux Map.empty
     pmap :: ProcessMap
     pmap = Map.fromList [ (pname, (map fst xs, p)) | (pname, xs, Just p) <- pdefs ]
 
+    newSession :: ChannelName -> IO (Endpoint, Endpoint)
+    newSession u = do
+      c <- newChan
+      d <- newChan
+      return (Endpoint u c d, Endpoint u d c)
+
     send :: Environment -> ChannelName -> Message -> IO ()
-    send σ u = writeChan (snd $ channel σ u)
+    send σ u | Endpoint _ _ x <- find σ u = writeChan x
 
     receive :: Environment -> ChannelName -> IO Message
-    receive σ u = readChan (fst $ channel σ u)
+    receive σ u | Endpoint _ x _ <- find σ u = readChan x
 
     forkChild :: IO () -> IO ThreadId
     forkChild io = do
@@ -104,55 +113,55 @@ run logging pdefs = aux Map.empty
           waitForChildren
 
     aux :: Environment -> Process -> IO ()
-    aux σ Done = waitForChildren
+    aux σ Done = waitForChildren >> log "done"
     aux σ (Call pname us) =
       case Map.lookup pname pmap of
         Nothing -> runtimeError $ "undefined process " ++ show pname
         Just (xs, p) -> do
           unless (length us == length xs) $ runtimeError $ "wrong number of arguments when invoking " ++ show pname
-          let σ' = foldr (uncurry Map.insert) σ (zip xs (map (channel σ) us))
+          let σ' = foldr (uncurry Map.insert) σ (zip xs (map (find σ) us))
           aux σ' p
     aux σ (Wait u p) = do
       m <- receive σ u
       case m of
         CloseM -> do
-          log $ "closed session " ++ show u
+          log $ "closed session " ++ show (find σ u)
           aux σ p
         _ -> runtimeError $ "wait: wrong message type from " ++ show u
     aux σ (Close u) = do
-      log $ "closing session " ++ show u
+      log $ "closing session " ++ show (find σ u)
       send σ u CloseM
     aux σ (Channel u Out v p) = do
-      log $ "sending channel " ++ show v ++ " on " ++ show u
-      send σ u (SessionM (channel σ v))
+      log $ "sending endpoint " ++ show (find σ v) ++ " on " ++ show (find σ u)
+      send σ u (EndpointM (find σ v))
       aux (Map.delete v σ) p
     aux σ (Channel u In x p) = do
       m <- receive σ u
       case m of
-        SessionM s -> do
-          log $ "received channel " ++ show x ++ " from " ++ show u
+        EndpointM s -> do
+          log $ "received endpoint " ++ show s ++ " from " ++ show (find σ u)
           aux (Map.insert x s σ) p
-        _ -> runtimeError $ "channel input: wrong message type from " ++ show u
+        _ -> runtimeError $ "endpoint input: wrong message type from " ++ show u
     aux σ (Label u Out ws gs) = do
       n <- randomInt (sum ws)
       let (tag, p) = gs!!pick n ws
-      log $ "sending label " ++ show tag ++ " on " ++ show u
+      log $ "sending label " ++ show tag ++ " on " ++ show (find σ u)
       send σ u (LabelM tag)
       aux σ p
     aux σ (Label u In _ gs) = do
       m <- receive σ u
       case m of
         LabelM tag -> do
-          log $ "received label " ++ show tag ++ " from " ++ show u
+          log $ "received label " ++ show tag ++ " from " ++ show (find σ u)
           case lookup tag gs of
             Just p -> aux σ p
             Nothing -> runtimeError $ "label input: unexpected label " ++ show tag
         _ -> runtimeError $ "label input: wrong message type from " ++ show u
     aux σ (New u _ p q) = do
-      ic <- newChan
-      oc <- newChan
-      forkChild (aux (Map.insert u (ic, oc) σ) p)
-      aux (Map.insert u (oc, ic) σ) q
+      (x, y) <- newSession u
+      log $ "creating session " ++ show x
+      forkChild (aux (Map.insert u x σ) p)
+      aux (Map.insert u y σ) q
     aux σ (Choice m p n q) = do
       i <- randomInt (m + n)
       log $ "performing an internal choice " ++ show (i < m)
@@ -162,4 +171,4 @@ run logging pdefs = aux Map.empty
     log :: String -> IO ()
     log msg = do
       t <- myThreadId
-      when logging $ printWarning $ show t ++ ": " ++ msg
+      when logging $ printWarning $ show t ++ " " ++ msg
